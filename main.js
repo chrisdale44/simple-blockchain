c34f212536f989bf405d35117bd790b4e98884da
Chris Dale <chris.dale@asos.com>
2018-01-26 14:38:30 +0000

    validate blockchain

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256');
+
+// Simple blockchain with validation
+// Limitations: 
+// - no mechanism to fix any broken blocks
+// - no p2p aspect
+// - no proof-of-work
+// - doesn't check funds before transaction
 
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
@@ -32,10 +39,34 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let chrisCoin = new Blockchain();
 chrisCoin.addBlock(new Block(1, "25/1/2018", { amount: 5 }));
 chrisCoin.addBlock(new Block(1, "26/1/2018", { amount: 10 }));
 
-console.log(JSON.stringify(chrisCoin, null, 4))
+console.log(JSON.stringify(chrisCoin, null, 4));
+
+console.log('Is blockchain valid? ' + chrisCoin.isChainValid())
+
+chrisCoin.chain[1].data = {amount: 100};
+chrisCoin.chain[1].hash = chrisCoin.chain[1].calculateHash();
+
+console.log('Is blockchain valid? ' + chrisCoin.isChainValid())
